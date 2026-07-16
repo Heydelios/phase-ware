77b6c8adcf569b2a63c92593de85561e613d5635
SwanSerre <adeliosswan@gmail.com>
2026-07-16 02:19:38 +0200

    added boat binted sprites and anims

--- MainScene/main.gd
+++ MainScene/main.gd
@@ -122,7 +122,7 @@ func load_next_minigame() -> void:
 func _on_intro_end() -> void:
 	state = main_state.MINIGAME_START
 	load_next_minigame()
-	next_minigame = load("res://Minigames/BingeEating/BingeEating.tscn").instantiate()
+	#next_minigame = load("res://Minigames/BenzDented/BenzDented.tscn").instantiate()
 	print("intro end")
 
 func _on_minigame_won() -> void:
--- Minigames/BenzDented/benz.gd
+++ Minigames/BenzDented/benz.gd
@@ -1,41 +1,43 @@
 extends TextureRect
 
-var start_pos := Vector2(100,580)
+var start_pos := Vector2(-100,514.0)
 var x_vel : float = 650
 var is_braking := false
 var is_stopped := false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	position = start_pos
-
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Minigame.get_game(self).game_ended:
 		return
-		
+	
 	position.x += x_vel*delta
 	
 	if position.x > %ParkingZone.position.x + %ParkingZone.size.x - size.x:
 		#Play loss anim crash
+		%BenzBroke.visible = true
+		%AnimPlayer.play("loss")
+		self_modulate = Color(1,1,1,0)
 		Minigame.lose_game(self)
-		return
 	
 	if is_braking:
 		x_vel *= .95
 		if x_vel < 5:
 			x_vel = 0
 			stop_check()
-			
 
 func stop_check() -> void:
 	if position.x > %ParkingZone.position.x:
 		#play win anim park success
+		%AnimPlayer.play("win")
 		Minigame.win_game(self)
 	else:
 		Minigame.lose_game(self)
-		#play_loss anim fail park
+		%AnimPlayer.play("loss_alt")
+		#play_loss anim fail park (early)
 
 func _unhandled_input(event):
 	if Minigame.get_game(self).game_ended:
